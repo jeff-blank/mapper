7ca6edbd62ff7792e515a642a2ec5f49e6c93e07
Jeff Blank <jeff-blank@users.noreply.github.com>
2020-11-19 19:20:00 -0800

    break mapper functions out of main.go and camelCase some names

---
+++ cmd/mapper/func.go
@@ -0,0 +1,263 @@
+package main
+
+import (
+	"database/sql"
+	"image"
+	"image/color"
+	"image/draw"
+	"io/ioutil"
+	re "regexp"
+	"strconv"
+	s "strings"
+	"time"
+
+	"github.com/golang/freetype"
+	"github.com/jeff-blank/mapper/pkg/config"
+	"github.com/jeff-blank/mapper/pkg/svgxml"
+	log "github.com/sirupsen/logrus"
+)
+
+// suck in count data
+func dbData(dbconfig map[string]string) (map[string]int, map[string]int) {
+
+	state_counts := make(map[string]int)
+	county_counts := make(map[string]int)
+
+	dbh, err := sql.Open(dbconfig["type"],
+		dbconfig["type"]+"://"+dbconfig["username"]+":"+
+			dbconfig["password"]+"@"+dbconfig["host"]+"/"+
+			dbconfig["name"]+dbconfig["connect_opts"])
+	if err != nil {
+		log.Fatal("sql.Open(): ", err)
+	}
+
+	query :=
+		"select " +
+			dbconfig["state_column"] + ", " +
+			dbconfig["county_column"] + ", " +
+			dbconfig["tally_column"] + " " +
+			"from " +
+			dbconfig["tables"] + " " +
+			dbconfig["where"] + " " +
+			dbconfig["group_by"]
+	log.Debug(query)
+	rows, err := dbh.Query(query)
+	if err != nil {
+		log.Fatal("dbh.Query(): ", err)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var state, county string
+		var count int
+		if err := rows.Scan(&state, &county, &count); err != nil {
+			log.Fatal("rows.Scan(): ", err)
+		}
+		state_counts[state] += count
+		state_county_key := s.Replace(state+" "+county, " ", "_", -1)
+		county_counts[state_county_key] = count
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal("rows.Err(): ", err)
+	}
+
+	return state_counts, county_counts
+
+}
+
+func colourSvgData(mapsvg_obj *svgxml.SVG, data map[string]int, re_fill *re.Regexp, colours map[string]string, mincount []int) (string, []string) {
+
+	var errors []string
+
+	for id, count := range data {
+		for _, mc := range mincount {
+			if count >= mc {
+				element := svgxml.FindPathById(mapsvg_obj, id)
+				if element != nil {
+					element.Style = string(re_fill.ReplaceAll([]byte(element.Style), []byte("${1}"+colours[strconv.Itoa(mc)])))
+				} else {
+					errors = append(errors, "'"+id+"' not found")
+				}
+			}
+		}
+	}
+	return string(svgxml.SVG2XML(mapsvg_obj, true)), errors
+}
+
+func annotate(img *image.RGBA, defaults config.LegendAnnotateParams, attrs config.MapSet, data map[string]int) {
+
+	ann_x := defaults.AnnotationX
+	ann_y := defaults.AnnotationY
+	timefmt := defaults.AnnotationTimeFmt
+	fontfile := defaults.AnnotationFontFile
+	fontsize := defaults.AnnotationFontSize
+	ann_str := defaults.AnnotationString
+
+	if attrs.LegendAnnotate.AnnotationX > 0 {
+		ann_x = attrs.LegendAnnotate.AnnotationX
+	}
+	if attrs.LegendAnnotate.AnnotationY > 0 {
+		ann_y = attrs.LegendAnnotate.AnnotationY
+	}
+
+	if len(attrs.LegendAnnotate.AnnotationFontFile) > 0 {
+		fontfile = attrs.LegendAnnotate.AnnotationFontFile
+	}
+
+	if attrs.LegendAnnotate.AnnotationFontSize > 0 {
+		fontsize = attrs.LegendAnnotate.AnnotationFontSize
+	}
+
+	if len(attrs.LegendAnnotate.AnnotationTimeFmt) > 0 {
+		timefmt = attrs.LegendAnnotate.AnnotationTimeFmt
+	}
+
+	if len(attrs.LegendAnnotate.AnnotationString) > 0 {
+		ann_str = attrs.LegendAnnotate.AnnotationString
+	}
+
+	fontdata, err := ioutil.ReadFile(fontfile)
+	if err != nil {
+		log.Fatalf("annotate(): read font file '%s': %v", fontfile, err)
+	}
+	font, err := freetype.ParseFont(fontdata)
+	if err != nil {
+		log.Fatal("annotate(): ParseFont(): ", err)
+	}
+
+	ft_ctx := freetype.NewContext()
+	ft_ctx.SetDPI(72.0)
+	ft_ctx.SetFont(font)
+	ft_ctx.SetFontSize(fontsize)
+	ft_ctx.SetClip(img.Bounds())
+	ft_ctx.SetDst(img)
+	ft_ctx.SetSrc(image.Black)
+	pt := freetype.Pt(int(ann_x), int(ann_y)+int(ft_ctx.PointToFixed(fontsize)>>6))
+
+	total_hits := 0
+	for _, hits := range data {
+		total_hits += hits
+	}
+	regions := len(data)
+	if attrs.RegionAdjustment != 0 {
+		regions += attrs.RegionAdjustment
+	}
+
+	annotation := s.Replace(ann_str, "%t%", strconv.Itoa(total_hits), -1)
+	annotation = s.Replace(annotation, "%c%", strconv.Itoa(regions), -1)
+	if s.Index(annotation, "%T%") >= 0 {
+		annotation = s.Replace(annotation, "%T%", time.Now().Format(timefmt), -1)
+	}
+	ann_lines := s.Split(annotation, "\n")
+	for _, line := range ann_lines {
+		_, err = ft_ctx.DrawString(line, pt)
+		if err != nil {
+			log.Fatal("annotate(): ft_ctx.DrawString(): ", err)
+		}
+		pt.Y += ft_ctx.PointToFixed(fontsize * 1.2)
+	}
+}
+
+func ahHatesLegends(img *image.RGBA, mincount []int, colours map[string]string, defaults config.LegendAnnotateParams, attrs config.MapSet) {
+	fontfile := defaults.LegendFontFile
+	fontsize := defaults.LegendFontSize
+	gravity := defaults.LegendGravity
+	orient := defaults.LegendOrient
+	cell_w := defaults.LegendCellWidth
+	cell_h := defaults.LegendCellHeight
+	cell_gap := defaults.LegendCellGap
+
+	if len(attrs.LegendAnnotate.LegendFontFile) > 0 {
+		fontfile = attrs.LegendAnnotate.LegendFontFile
+	}
+
+	if attrs.LegendAnnotate.LegendFontSize > 0 {
+		fontsize = attrs.LegendAnnotate.LegendFontSize
+	}
+
+	if len(attrs.LegendAnnotate.LegendGravity) > 0 {
+		gravity = attrs.LegendAnnotate.LegendGravity
+	}
+
+	if len(attrs.LegendAnnotate.LegendOrient) > 0 {
+		orient = attrs.LegendAnnotate.LegendOrient
+	}
+
+	if attrs.LegendAnnotate.LegendCellWidth > 0 {
+		cell_w = attrs.LegendAnnotate.LegendCellWidth
+	}
+	if attrs.LegendAnnotate.LegendCellHeight > 0 {
+		cell_h = attrs.LegendAnnotate.LegendCellHeight
+	}
+	if attrs.LegendAnnotate.LegendCellGap > 0 {
+		cell_gap = attrs.LegendAnnotate.LegendCellGap
+	}
+
+	fontdata, err := ioutil.ReadFile(fontfile)
+	if err != nil {
+		log.Fatalf("ahHatesLegends(): read font file '%s': %v", fontfile, err)
+	}
+	font, err := freetype.ParseFont(fontdata)
+	if err != nil {
+		log.Fatal("ahHatesLegends(): ParseFont(): ", err)
+	}
+	b := img.Bounds()
+	ft_ctx := freetype.NewContext()
+	ft_ctx.SetDPI(72.0)
+	ft_ctx.SetFont(font)
+	ft_ctx.SetFontSize(fontsize)
+	ft_ctx.SetClip(b)
+	ft_ctx.SetDst(img)
+	ft_ctx.SetSrc(image.Black)
+
+	legend_width := cell_w
+	legend_height := cell_h
+	if orient == "vertical" {
+		legend_height = len(colours)*(cell_h+cell_gap) - cell_gap
+	} else {
+		legend_width = len(colours)*(cell_w+cell_gap) - cell_gap
+	}
+
+	box_x := 0
+	box_y := 0
+	if s.ToLower(gravity)[0] == 's' {
+		box_y = b.Dy() - legend_height
+	}
+	if s.ToLower(gravity)[1] == 'e' {
+		box_x = b.Dx() - legend_width
+	}
+
+	for i, mc := range mincount {
+		c_red, _ := strconv.ParseUint(colours[strconv.Itoa(mc)][0:2], 16, 8)
+		c_green, _ := strconv.ParseUint(colours[strconv.Itoa(mc)][2:4], 16, 8)
+		c_blue, _ := strconv.ParseUint(colours[strconv.Itoa(mc)][4:6], 16, 8)
+		fill := color.RGBA{uint8(c_red), uint8(c_green), uint8(c_blue), 255}
+		draw.Draw(img, image.Rect(box_x, box_y, box_x+cell_w, box_y+cell_h),
+			&image.Uniform{fill}, image.ZP, draw.Src)
+		if orient == "vertical" {
+			box_y += cell_h + cell_gap
+		} else {
+			box_x += cell_w + cell_gap
+		}
+
+		label := strconv.Itoa(mc)
+		if i == len(mincount)-1 {
+			label = label + "+"
+		} else if mincount[i+1] != (mc + 1) {
+			label = label + "-" + strconv.Itoa(mincount[i+1]-1)
+		}
+		var text_x, text_y int
+		if orient == "vertical" {
+			text_x = box_x + 4
+			text_y = box_y - cell_h + int(ft_ctx.PointToFixed(fontsize)>>6)
+		} else {
+			text_x = box_x - cell_w + 4
+			text_y = box_y + int(ft_ctx.PointToFixed(fontsize)>>6)
+		}
+		fpt := freetype.Pt(text_x, text_y)
+		_, err = ft_ctx.DrawString(label, fpt)
+		if err != nil {
+			log.Fatal("ahHatesLegends(): ft_ctx.DrawString(): ", err)
+		}
+	}
+}
--- cmd/mapper/main.go
+++ cmd/mapper/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"fmt"
 	"image"
-	"image/color"
 	"image/draw"
 	"image/png"
 	"io"
@@ -17,9 +15,7 @@ import (
 	"strconv"
 	s "strings"
 	"sync"
-	"time"
 
-	"github.com/golang/freetype"
 	"github.com/jeff-blank/mapper/pkg/config"
 	"github.com/jeff-blank/mapper/pkg/svgxml"
 	_ "github.com/lib/pq"
@@ -32,251 +28,6 @@ type IntArray []int
 func (list IntArray) Len() int           { return len(list) }
 func (list IntArray) Swap(a, b int)      { list[a], list[b] = list[b], list[a] }
 func (list IntArray) Less(a, b int) bool { return list[a] < list[b] }
-
-// suck in count data
-func db_data(dbconfig map[string]string) (map[string]int, map[string]int) {
-
-	state_counts := make(map[string]int)
-	county_counts := make(map[string]int)
-
-	dbh, err := sql.Open(dbconfig["type"],
-		dbconfig["type"]+"://"+dbconfig["username"]+":"+
-			dbconfig["password"]+"@"+dbconfig["host"]+"/"+
-			dbconfig["name"]+dbconfig["connect_opts"])
-	if err != nil {
-		log.Fatal("sql.Open(): ", err)
-	}
-
-	query :=
-		"select " +
-			dbconfig["state_column"] + ", " +
-			dbconfig["county_column"] + ", " +
-			dbconfig["tally_column"] + " " +
-			"from " +
-			dbconfig["tables"] + " " +
-			dbconfig["where"] + " " +
-			dbconfig["group_by"]
-	log.Debug(query)
-	rows, err := dbh.Query(query)
-	if err != nil {
-		log.Fatal("dbh.Query(): ", err)
-	}
-
-	defer rows.Close()
-	for rows.Next() {
-		var state, county string
-		var count int
-		if err := rows.Scan(&state, &county, &count); err != nil {
-			log.Fatal("rows.Scan(): ", err)
-		}
-		state_counts[state] += count
-		state_county_key := s.Replace(state+" "+county, " ", "_", -1)
-		county_counts[state_county_key] = count
-	}
-	if err := rows.Err(); err != nil {
-		log.Fatal("rows.Err(): ", err)
-	}
-
-	return state_counts, county_counts
-
-}
-
-func colour_svgdata(mapsvg_obj *svgxml.SVG, data map[string]int, re_fill *re.Regexp, colours map[string]string, mincount []int) (string, []string) {
-
-	var errors []string
-
-	for id, count := range data {
-		for _, mc := range mincount {
-			if count >= mc {
-				element := svgxml.FindPathById(mapsvg_obj, id)
-				if element != nil {
-					element.Style = string(re_fill.ReplaceAll([]byte(element.Style), []byte("${1}"+colours[strconv.Itoa(mc)])))
-				} else {
-					errors = append(errors, "'"+id+"' not found")
-				}
-			}
-		}
-	}
-	return string(svgxml.SVG2XML(mapsvg_obj, true)), errors
-}
-
-func annotate(img *image.RGBA, defaults config.LegendAnnotateParams, attrs config.MapSet, data map[string]int) {
-
-	ann_x := defaults.AnnotationX
-	ann_y := defaults.AnnotationY
-	timefmt := defaults.AnnotationTimeFmt
-	fontfile := defaults.AnnotationFontFile
-	fontsize := defaults.AnnotationFontSize
-	ann_str := defaults.AnnotationString
-
-	if attrs.LegendAnnotate.AnnotationX > 0 {
-		ann_x = attrs.LegendAnnotate.AnnotationX
-	}
-	if attrs.LegendAnnotate.AnnotationY > 0 {
-		ann_y = attrs.LegendAnnotate.AnnotationY
-	}
-
-	if len(attrs.LegendAnnotate.AnnotationFontFile) > 0 {
-		fontfile = attrs.LegendAnnotate.AnnotationFontFile
-	}
-
-	if attrs.LegendAnnotate.AnnotationFontSize > 0 {
-		fontsize = attrs.LegendAnnotate.AnnotationFontSize
-	}
-
-	if len(attrs.LegendAnnotate.AnnotationTimeFmt) > 0 {
-		timefmt = attrs.LegendAnnotate.AnnotationTimeFmt
-	}
-
-	if len(attrs.LegendAnnotate.AnnotationString) > 0 {
-		ann_str = attrs.LegendAnnotate.AnnotationString
-	}
-
-	fontdata, err := ioutil.ReadFile(fontfile)
-	if err != nil {
-		log.Fatalf("annotate(): read font file '%s': %v", fontfile, err)
-	}
-	font, err := freetype.ParseFont(fontdata)
-	if err != nil {
-		log.Fatal("annotate(): ParseFont(): ", err)
-	}
-
-	ft_ctx := freetype.NewContext()
-	ft_ctx.SetDPI(72.0)
-	ft_ctx.SetFont(font)
-	ft_ctx.SetFontSize(fontsize)
-	ft_ctx.SetClip(img.Bounds())
-	ft_ctx.SetDst(img)
-	ft_ctx.SetSrc(image.Black)
-	pt := freetype.Pt(int(ann_x), int(ann_y)+int(ft_ctx.PointToFixed(fontsize)>>6))
-
-	total_hits := 0
-	for _, hits := range data {
-		total_hits += hits
-	}
-	regions := len(data)
-	if attrs.RegionAdjustment != 0 {
-		regions += attrs.RegionAdjustment
-	}
-
-	annotation := s.Replace(ann_str, "%t%", strconv.Itoa(total_hits), -1)
-	annotation = s.Replace(annotation, "%c%", strconv.Itoa(regions), -1)
-	if s.Index(annotation, "%T%") >= 0 {
-		annotation = s.Replace(annotation, "%T%", time.Now().Format(timefmt), -1)
-	}
-	ann_lines := s.Split(annotation, "\n")
-	for _, line := range ann_lines {
-		_, err = ft_ctx.DrawString(line, pt)
-		if err != nil {
-			log.Fatal("annotate(): ft_ctx.DrawString(): ", err)
-		}
-		pt.Y += ft_ctx.PointToFixed(fontsize * 1.2)
-	}
-}
-
-func ah_hates_legends(img *image.RGBA, mincount []int, colours map[string]string, defaults config.LegendAnnotateParams, attrs config.MapSet) {
-	fontfile := defaults.LegendFontFile
-	fontsize := defaults.LegendFontSize
-	gravity := defaults.LegendGravity
-	orient := defaults.LegendOrient
-	cell_w := defaults.LegendCellWidth
-	cell_h := defaults.LegendCellHeight
-	cell_gap := defaults.LegendCellGap
-
-	if len(attrs.LegendAnnotate.LegendFontFile) > 0 {
-		fontfile = attrs.LegendAnnotate.LegendFontFile
-	}
-
-	if attrs.LegendAnnotate.LegendFontSize > 0 {
-		fontsize = attrs.LegendAnnotate.LegendFontSize
-	}
-
-	if len(attrs.LegendAnnotate.LegendGravity) > 0 {
-		gravity = attrs.LegendAnnotate.LegendGravity
-	}
-
-	if len(attrs.LegendAnnotate.LegendOrient) > 0 {
-		orient = attrs.LegendAnnotate.LegendOrient
-	}
-
-	if attrs.LegendAnnotate.LegendCellWidth > 0 {
-		cell_w = attrs.LegendAnnotate.LegendCellWidth
-	}
-	if attrs.LegendAnnotate.LegendCellHeight > 0 {
-		cell_h = attrs.LegendAnnotate.LegendCellHeight
-	}
-	if attrs.LegendAnnotate.LegendCellGap > 0 {
-		cell_gap = attrs.LegendAnnotate.LegendCellGap
-	}
-
-	fontdata, err := ioutil.ReadFile(fontfile)
-	if err != nil {
-		log.Fatalf("ah_hates_legends(): read font file '%s': %v", fontfile, err)
-	}
-	font, err := freetype.ParseFont(fontdata)
-	if err != nil {
-		log.Fatal("ah_hates_legends(): ParseFont(): ", err)
-	}
-	b := img.Bounds()
-	ft_ctx := freetype.NewContext()
-	ft_ctx.SetDPI(72.0)
-	ft_ctx.SetFont(font)
-	ft_ctx.SetFontSize(fontsize)
-	ft_ctx.SetClip(b)
-	ft_ctx.SetDst(img)
-	ft_ctx.SetSrc(image.Black)
-
-	legend_width := cell_w
-	legend_height := cell_h
-	if orient == "vertical" {
-		legend_height = len(colours)*(cell_h+cell_gap) - cell_gap
-	} else {
-		legend_width = len(colours)*(cell_w+cell_gap) - cell_gap
-	}
-
-	box_x := 0
-	box_y := 0
-	if s.ToLower(gravity)[0] == 's' {
-		box_y = b.Dy() - legend_height
-	}
-	if s.ToLower(gravity)[1] == 'e' {
-		box_x = b.Dx() - legend_width
-	}
-
-	for i, mc := range mincount {
-		c_red, _ := strconv.ParseUint(colours[strconv.Itoa(mc)][0:2], 16, 8)
-		c_green, _ := strconv.ParseUint(colours[strconv.Itoa(mc)][2:4], 16, 8)
-		c_blue, _ := strconv.ParseUint(colours[strconv.Itoa(mc)][4:6], 16, 8)
-		fill := color.RGBA{uint8(c_red), uint8(c_green), uint8(c_blue), 255}
-		draw.Draw(img, image.Rect(box_x, box_y, box_x+cell_w, box_y+cell_h),
-			&image.Uniform{fill}, image.ZP, draw.Src)
-		if orient == "vertical" {
-			box_y += cell_h + cell_gap
-		} else {
-			box_x += cell_w + cell_gap
-		}
-
-		label := strconv.Itoa(mc)
-		if i == len(mincount)-1 {
-			label = label + "+"
-		} else if mincount[i+1] != (mc + 1) {
-			label = label + "-" + strconv.Itoa(mincount[i+1]-1)
-		}
-		var text_x, text_y int
-		if orient == "vertical" {
-			text_x = box_x + 4
-			text_y = box_y - cell_h + int(ft_ctx.PointToFixed(fontsize)>>6)
-		} else {
-			text_x = box_x - cell_w + 4
-			text_y = box_y + int(ft_ctx.PointToFixed(fontsize)>>6)
-		}
-		fpt := freetype.Pt(text_x, text_y)
-		_, err = ft_ctx.DrawString(label, fpt)
-		if err != nil {
-			log.Fatal("ah_hates_legends(): ft_ctx.DrawString(): ", err)
-		}
-	}
-}
 
 func main() {
 
@@ -297,7 +48,7 @@ func main() {
 	// make sorted list of keys (minimum counts) for later comparisons
 	mincount := make([]int, len(cfg.Colours))
 	i := 0
-	for k, _ := range cfg.Colours {
+	for k := range cfg.Colours {
 		k_i, _ := strconv.ParseInt(k, 0, 64)
 		mincount[i] = int(k_i)
 		i++
@@ -315,7 +66,7 @@ func main() {
 		log.Fatal("re.Compile() .svg: ", err)
 	}
 
-	state_data, county_data := db_data(cfg.DbParam)
+	state_data, county_data := dbData(cfg.DbParam)
 
 	for maptype, mapset := range cfg.Maps {
 		var data map[string]int
@@ -339,7 +90,7 @@ func main() {
 						newDbConfig[k] = v
 					}
 					newDbConfig["where"] = cfg.DbParam["where"] + " and " + attrs.DbWhere
-					state_new, county_new := db_data(newDbConfig)
+					state_new, county_new := dbData(newDbConfig)
 					if maptype == "states" {
 						mapdata = state_new
 					} else {
@@ -412,7 +163,7 @@ func main() {
 					mapdata = county_data_new
 				}
 
-				svg_coloured, errlist := colour_svgdata(mapsvg_obj, mapdata, re_fill, cfg.Colours, mincount)
+				svg_coloured, errlist := colourSvgData(mapsvg_obj, mapdata, re_fill, cfg.Colours, mincount)
 				if len(errlist) > 0 {
 					for _, errmsg := range errlist {
 						log.Warnf("%s: %s\n", attrs.InputFile, errmsg)
@@ -448,20 +199,20 @@ func main() {
 					png_reader := s.NewReader(string(png_data))
 					img, _, err := image.Decode(png_reader)
 					b := img.Bounds()
-					img_rgba := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
-					draw.Draw(img_rgba, img_rgba.Bounds(), img, b.Min, draw.Src)
+					imgRbga := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+					draw.Draw(imgRbga, imgRbga.Bounds(), img, b.Min, draw.Src)
 
 					if len(cfg.LADefaults.LegendFontFile) > 0 || len(attrs.LegendAnnotate.LegendFontFile) > 0 {
-						ah_hates_legends(img_rgba, mincount, cfg.Colours, cfg.LADefaults, attrs)
+						ahHatesLegends(imgRbga, mincount, cfg.Colours, cfg.LADefaults, attrs)
 					}
 
-					annotate(img_rgba, cfg.LADefaults, attrs, mapdata)
+					annotate(imgRbga, cfg.LADefaults, attrs, mapdata)
 					outfile_handle, err := os.Create(attrs.OutputFile)
 					if err != nil {
 						log.Errorf("can't create '%s': %v", attrs.OutputFile, err)
 						return
 					}
-					if err := png.Encode(outfile_handle, img_rgba); err != nil {
+					if err := png.Encode(outfile_handle, imgRbga); err != nil {
 						outfile_handle.Close()
 						log.Fatalf("close png file '%s': %v", attrs.OutputFile, err)
 					}
@@ -479,5 +230,4 @@ func main() {
 	}
 
 	wg.Wait()
-
 }
